a0b82afb2e795256e4763ed4f60c4262107ec67b
anagomezmignot <ana.gomezmignot@gmail.com>
2022-05-27 17:01:02 -0300

    cree archivo respuestas

---
+++ respuestas.js
@@ -0,0 +1,3 @@
+Git es un proyecto de código abierto maduro y con un mantenimiento activo que desarrolló originalmente Linus Torvalds, el famoso creador del kernel del sistema operativo Linux, en 2005.
+Microsoft compró GitHub
+abrir el Terminal en Mac, o el "Command Prompt" o "Powershell" en Windows
